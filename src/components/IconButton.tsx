113b78567f0a7e6773e28a1430bc6b80036ca6c0
msemonlab <msemonhq@gmail.com>
2026-09-24 22:28:48 +0600

    fix: resolve ESLint issues causing GitHub Actions build failure

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function IconButton({ className = '', children, ...props }: IconButtonProps) {
   return (
